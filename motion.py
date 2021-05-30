bcd71e72c3e5f3e4c3856032dc9baf5de1524bad
kavinteng <76694895+kavinteng@users.noreply.github.com>
2021-05-30 21:28:03 +0700

    Add files via upload

--- motion.py
+++ motion.py
@@ -1,5 +1,5 @@
 import datetime
-import time
+import numpy as np
 import cv2
 import imutils
 
@@ -9,12 +9,15 @@ frame_height = int(cap.get(4)/2)
 firstFrame = None
 
 while True:
-    ret, frame = cap.read()
+    _, frame = cap.read()
     text = "Unoccupied"
-    # decrease_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-    decrease_frame = cv2.resize(frame,(frame_width, frame_height))
-    gray = cv2.cvtColor(decrease_frame, cv2.COLOR_RGB2GRAY)
-    gray = cv2.GaussianBlur(frame, (21, 21), 0)
+    if frame is None:
+        break
+    frame = cv2.resize(frame, (frame_width, frame_height))
+    # decrease_frame = cv2.resize(frame,(frame_width, frame_height))
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    # print(decrease_frame.shape, gray.shape)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
     if firstFrame is None:
         firstFrame = gray
@@ -23,9 +26,9 @@ while True:
     frameDelta = cv2.absdiff(firstFrame, gray)
     firstFrame = gray
     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-    thresh = cv2.cvtColor(thresh,cv2.COLOR_RGB2GRAY)
 
-    thresh = cv2.dilate(thresh, None, iterations=2)
+    kernel = np.ones((8, 8), np.uint8)
+    thresh = cv2.dilate(thresh, kernel, iterations=8)
     cnts= cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
 
@@ -33,21 +36,15 @@ while True:
         if cv2.contourArea(c) < 500:
             continue
         (x, y, w, h) = cv2.boundingRect(c)
-        # x *= 4
-        # y *= 4
-        # w *= 4
-        # h *= 4
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         text = "Occupied"
-        cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-                    (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+        cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
         cv2.imshow("Security Feed", frame)
         cv2.imshow("Thresh", thresh)
         cv2.imshow("Frame Delta", frameDelta)
-        k = cv2.waitKey(1)
-        if k == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
